290707bfc7419a11ca2ebce015add9d75f03eb6a
Mubeena Pathan(https://future-bloom-desk.lovable.app/) <pathanmubeena83@gmail.com>
2026-01-24 12:32:58 +0530

    Time: 5 ms (73.58%), Space: 46.1 MB (25.24%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,15 @@
 class Solution {
-    public boolean isPalindrome(int n) {
-        if(n<0)
+    public boolean isPalindrome(int num) {
+         if(num<0)
         return false;
-     int rev=0;
-     int temp=n;
-     while(temp!=0){
-        int digit=temp%10;
-        temp=temp/10;
-        rev=rev*10+digit;
-     }
-     return(rev==n);
+        int rev=0;
+        int temp=num;
+        while(temp!=0){
+            int digit=temp%10;
+            temp=temp/10;
+            rev=rev*10+digit;
+        }
+        return (rev==num);
         
     }
 }
