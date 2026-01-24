acb120a99005997f187d419eb8c42c9e2c2db667
Mubeena Pathan(https://future-bloom-desk.lovable.app/) <pathanmubeena83@gmail.com>
2026-01-24 12:22:40 +0530

    Time: 1 ms (99.74%), Space: 42.7 MB (54.48%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,16 +1,18 @@
 class Solution {
     public int reverse(int n) {
-        long rev=0;
-        int temp=n;
-        while(temp!=0){
-            int digit=temp%10;
-            temp=temp/10;
-            rev=rev*10+digit;
+        long rev = 0;
+        int temp = n;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            temp = temp / 10;
+            rev = rev * 10 + digit;
         }
-        if(rev>Integer.MAX_VALUE||rev<Integer.MIN_VALUE){
+
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
             return 0;
         }
-        return (int)rev;
-        
+
+        return (int) rev;
     }
 }
