4df9c3212ed849048ae0202f09146ebdb7f626da
Mubeena Pathan(https://future-bloom-desk.lovable.app/) <pathanmubeena83@gmail.com>
2026-02-01 22:56:38 +0530

    Time: 1 ms (77.11%), Space: 46.6 MB (76.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if (nums.length == 0) return 0; 
+      
         
         int j = 0; 
         for (int i = 1; i < nums.length; i++) {
@@ -9,6 +9,6 @@ class Solution {
                 nums[j] = nums[i];
             }
         }
-        return j + 1; 
+        return j+1; 
     }
 }
