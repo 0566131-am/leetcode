5c449fa207aa797377246eba2589c3473a8252d1
Mubeena Pathan(https://future-bloom-desk.lovable.app/) <pathanmubeena83@gmail.com>
2026-02-25 20:33:58 +0530

    Time: 0 ms (100%), Space: 43.7 MB (23.71%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,32 +1,29 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                count0++;
-            } else if (nums[i] == 1) {
-                count1++;
-            } else if (nums[i] == 2) {
-                count2++;
-            }
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<nums.length;i++){
+        if(nums[i]==0){
+             count0++;
         }
-
-    
-        int idx = 0;
-
-        for (int i = 0; i < count0; i++) {
-            nums[idx++] = 0;
+        else if(nums[i]==1){
+            count1++;
         }
-
-        for (int i = 0; i < count1; i++) {
-            nums[idx++] = 1;
+        else{
+        count2++;
         }
-
-        for (int i = 0; i < count2; i++) {
-            nums[idx++] = 2;
+        }
+        int idx=0;
+        for(int i=0;i<count0;i++){
+            nums[idx++]=0;
+        }
+        for(int i=0;i<count1;i++){
+            nums[idx++]=1;
+        }
+        for(int i=0;i<count2;i++){
+            nums[idx++]=2;
         }
     }
+
 }
