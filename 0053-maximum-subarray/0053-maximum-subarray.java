62d98b592b8551030fe170ebccc9340ae09417bc
Mubeena Pathan(https://future-bloom-desk.lovable.app/) <pathanmubeena83@gmail.com>
2026-02-26 17:33:26 +0530

    Time: 1 ms (99.72%), Space: 77.1 MB (83.88%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,11 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max=nums[0];
-        int cur=nums[0];
+       
+       int sum=nums[0];
+       int max=nums[0];
         for(int i=1;i<nums.length;i++){
-            cur=Math.max(nums[i],cur+nums[i]);
-            max=Math.max(max,cur);
+            sum=Math.max(nums[i],sum+nums[i]);
+            max=Math.max(max,sum);
         }
         return max;
+       
     }
 }
