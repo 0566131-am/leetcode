6e7b67c6cd2fbb10fd225a14949d1c03dc5143fd
Mubeena Pathan <pathanmubeena83@gmail.com>
2026-04-06 20:51:03 +0530

    Time: 1 ms (62.79%), Space: 42.4 MB (79.37%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,11 +1,11 @@
 class Solution {
     public int reverseBits(int n) {
-        String str=Integer.toBinaryString(n);
-        StringBuilder s=new StringBuilder(str);
-        while(s.length()<32){
-            s.insert(0,"0");
+        int result=0;
+        for(int i=0;i<32;i++){
+            result<<=1;
+            result|=(n&1);
+            n>>=1;
         }
-        int res=Integer.parseInt(s.reverse().toString(),2);
-        return res;
+        return result;
     }
 }
