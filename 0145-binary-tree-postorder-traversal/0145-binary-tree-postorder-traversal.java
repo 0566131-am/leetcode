5e6e75a4cb43e9fc91fc186bb7fd15daabc136f7
Mubeena Pathan <pathanmubeena83@gmail.com>
2026-06-06 08:24:20 +0530

    Time: 0 ms (100%), Space: 42.8 MB (95.89%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        if (root == null) return result;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            result.addFirst(current.val);
+            
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+        }
+        
+        return result;
+    }
+}
